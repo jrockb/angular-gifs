e4afff2b7c01258c527936e84f46440a09bb0a7b
jrockb87 <jrockb@gmail.com>
2021-06-01 16:28:48 -0500

    agregada funcionalidad localstoprage para persistir las busquedas localmente

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,16 +12,25 @@ export class GifsService {
 
   public resultados: Gif[] = []; // es publica porque no importa que se modifique fuera de esta clase
 
+  constructor(private http: HttpClient){ // para hacer peticiones http en base a observables
+    // como el constructor solo se ejecuta una vez se puede usar para cargar la data de localStorage
+    if (localStorage.getItem('historial')){ // validar si el elemento no es nulo
+      // tslint:disable-next-line: no-non-null-assertion
+      this._historial = JSON.parse(localStorage.getItem('historial')!); // ! indica que se obvie el error
+      // ya que se ha validado que el objeto no es nulo
+    }
+  }
+
   get historial(): string[]{
     return [...this._historial]; // rompe la refrencia para devolver una copia de ese arreglo
   }
-
-  constructor(private http: HttpClient){} // para hacer peticiones http en base a observables
 
   buscarGifs(query: string): void{
     query = query.trim().toLowerCase(); // para que todo se procese en minusculas
     if (!this._historial.includes(query)){ // metodo que busca si este elemento ya existe en el arreglo
       this._historial.unshift(query); // inserta el elemento en la primera posición
+      localStorage.setItem('historial', JSON.stringify(this._historial)); // para que se persista la información localmente
+      // el metodo JSON.stringfy genera un string de un objeto
     }
     this._historial = this._historial.splice(0, 10); // corta el arreglo para que tenga 10 elementos
     // el modulo http retorna un observable
