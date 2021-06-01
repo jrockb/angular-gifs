357027602650112bbd8c858a383ff93ffbf066c5
jrockb87 <jrockb@gmail.com>
2021-06-01 14:13:25 -0500

    agregada interfaz con las propiedades del objeto json de la respuesta de giphy y agregado tipado a objetos de respuesta en el service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class GifsService {
   private apiKey: string = 'EdT0FqLUd3lq10w0eL5z0FIvvAe3fn5u';
   private _historial: string[] = [];
 
-  public resultados: any[] = []; // es publica porque no importa que se modifique fuera de esta clase
+  public resultados: Gif[] = []; // es publica porque no importa que se modifique fuera de esta clase
 
   get historial(): string[]{
     return [...this._historial]; // rompe la refrencia para devolver una copia de ese arreglo
@@ -24,8 +25,8 @@ export class GifsService {
     }
     this._historial = this._historial.splice(0, 10); // corta el arreglo para que tenga 10 elementos
     // el modulo http retorna un observable
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=EdT0FqLUd3lq10w0eL5z0FIvvAe3fn5u&q=${query}&limit=10`)
-      .subscribe( (resp: any) => {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=EdT0FqLUd3lq10w0eL5z0FIvvAe3fn5u&q=${query}&limit=10`)
+      .subscribe( (resp) => {
       this.resultados = resp.data;
       console.log(resp.data);
     }); // el subscribe se ejecuta cuando se tenga la resolución del get
