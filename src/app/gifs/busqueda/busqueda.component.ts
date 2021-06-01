f5fa1e6d85b3e83251c36ab675a21102607411d7
jrockb87 <jrockb@gmail.com>
2021-06-01 10:20:36 -0500

    uso de viewchild

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent{
 
-  constructor() { }
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>; // va a buscar en el html un elemento con esa referencia
+  // !: indica que el objeto nunca será nulo, para que no se tenga el error
+  buscar(): void{
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = '';
+    // document.querySelector('input').value = ''; usando javascript para borrar la caja de texto cuando se realiza la busqueda
 
-  ngOnInit(): void {
   }
+
 
 }
