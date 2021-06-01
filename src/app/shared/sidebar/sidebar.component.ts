75f0d4d131b8d6d414803d657c372836cefa3b16
jrockb87 <jrockb@gmail.com>
2021-06-01 11:07:24 -0500

    se imprime historial de busqueda en el sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
-  constructor() { }
+  constructor(private gifsService: GifsService) { }
+
+  get historial(): string[]{ // obtiene la información del historial que viene del servicio
+    return this.gifsService.historial;
+  }
 
 }
